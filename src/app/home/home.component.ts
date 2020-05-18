50b947854e57af48a50df985090599d0533d0b7d
Manoel Almeida <manoel.almeida@bandtec.com.br>
2020-05-18 15:51:34 -0300

    Adding map view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
+import { registerElement } from "nativescript-angular/element-registry";
+import { MapView } from "nativescript-google-maps-sdk";
+
+registerElement("MapView", () => MapView);
 
 @Component({
     selector: "Home",
@@ -6,11 +10,28 @@ import { Component, OnInit } from "@angular/core";
 })
 export class HomeComponent implements OnInit {
 
+    latitude: -23.4188119;
+    longitude: -46.7171601;
+    zoom: 8;
+    bearing: 0;
+    tilt: 0;
+    mapAnimationsEnabled: true;
+    padding: 0;
+
     constructor() {
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
         // Init your component properties here.
+    }
+
+    onMapReady(event) {
+        let mapView = event.object as MapView;
+
+        mapView.latitude = -23.4188967;
+        mapView.longitude = -46.7172574;
+
+        mapView.zoom = 17;
     }
 }
